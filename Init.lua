f0b487e4b1d6622bb2a6cb0d3ca1fdf96283f570
Alex S S Fonseca <alexkads@gmail.com>
2024-12-31 07:51:12 -0300

    Fix

--- Init.lua
+++ Init.lua
@@ -1,7 +1,9 @@
 local PhotoObserver = require "PhotoObserver"
+local LrDialogs = import "LrDialogs"
 
 local function initPlugin()
   PhotoObserver.startMonitoring()
+  LrDialogs.message("Plugin carregado corretamente.", "O plugin foi carregado com sucesso.", "info")
 end
 
 return {
